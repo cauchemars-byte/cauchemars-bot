7552110914a1b5c5297d2b1edefca5041f45e75d
cauchemars-byte <lamjoledakhoui@gmail.com>
2026-06-16 05:01:33 +0200

    Update Puppeteer executable path in index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
+        executablePath: '/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.88/chrome-linux64/chrome',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
