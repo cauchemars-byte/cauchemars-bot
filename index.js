ec97381fc42dbaef04f4080bffd3ec265707b309
cauchemars-byte <lamjoledakhoui@gmail.com>
2026-06-16 04:49:39 +0200

    Set Puppeteer to run in headless mode

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        executablePath: '/usr/bin/google-chrome',
+        headless: true,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
