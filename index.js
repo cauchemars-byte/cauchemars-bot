26b91dd07b13f81e4b09a3c46456c200ecb1d3f2
cauchemars-byte <lamjoledakhoui@gmail.com>
2026-06-16 04:42:02 +0200

    Update Puppeteer options for Chrome executable

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        executablePath: '/usr/bin/google-chrome',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ]
     }
 });
 
